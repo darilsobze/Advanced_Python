3825ccc90852a3a80325d33f240a14f02770d62a
darilsobze <darilsobze13@icloud.com>
2025-10-25 21:53:04 +0200

    List Comprehensions

---
+++ list_comprehensions.py
@@ -0,0 +1,13 @@
+# Lists Comprehensions
+my_list = [i ** 2 for i in range(10)]
+print(my_list)
+
+fruits = ["Banana", "Lemon", "Orange", "apple", "Birne"]
+list2 = [fruit for fruit in fruits if fruit[0] == "B"]
+print(list2)
+
+prices = [25, 2, 3, 48]
+
+# Nested List Comprehensions
+fruits_prices = [[fruit, price] for fruit in fruits for price in prices]
+print(fruits_prices)
